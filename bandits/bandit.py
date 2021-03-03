98271a34be96391147dcd01a29bdd8639d38cec4
oliver <oliver.burkhard@gmail.com>
2021-03-03 09:48:48 +0100

    Allow sequential play

--- bandits/bandit.py
+++ bandits/bandit.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 import numpy as np
 
 from bandits.arms import Arm
@@ -9,19 +11,32 @@ class Bandit:
 
         - __init__(arms: dict)              # set up bandit with a dict of named arms
         - options() -> list[str]            # returns the names of the currently available arms
-        - play(context, choices) -> np.array, np.array   # given contexts and a list of played arm-names for
-                                                           each context, return value and regret
+        - play_all(context, choices) -> np.array, np.array   # given contexts and a list of played arm-names for
+                                                               each context, return value and regret
+        - play_one(context, choice) -> float, float  # given a single context gets observed value and regret
     """
     def __init__(self, arms: dict[str, Arm]):
         """
         :param arms: Arms to be used at initialisation
         """
-        self.arms = arms
+        self.arms = OrderedDict(arms)
 
     def options(self):
         return list(self.arms.keys())
 
-    def play(self, contexts: np.array, choices: list):
+    def play_one(self, context: np.array, choice: str):
+        assert choice in self.arms.keys()
+        arms = list(self.arms.keys())
+        all_obs = np.zeros(len(arms))
+        all_ev = np.zeros(len(arms))
+        for i, arm in enumerate(arms):
+            obs, ev = self.arms[arm].value(context.reshape([1, -1]))
+            all_obs[i] = obs
+            all_ev[i] = ev
+        ind_choice = arms.index(choice)
+        return all_obs[ind_choice], max(all_ev) - all_ev[ind_choice]
+
+    def play_all(self, contexts: np.array, choices: list):
         assert contexts.shape[0] == len(choices), "All contexts must have a choice"
         n, d = contexts.shape
         arms, obs, ev = self._get_values(contexts)
